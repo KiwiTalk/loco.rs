b503054790718e06dad9a07516ce0b3d4f136aec
Sangwon Ryu <bdboy090@naver.com>
2020-05-08 13:08:46 +0900

    Declare structs for login

--- src/types/structs/login.rs
+++ src/types/structs/login.rs
@@ -1,4 +1,18 @@
 // Maps to LoginAccessDataStruct
 pub struct LoginAccessData {
-
+    status: i32,
+    story_url: String,
+    user_id: i32,
+    country_iso: String,
+    country_code: String,
+    account_id: i32,
+    server_time: i32,
+    reset_user_data: bool,
+    access_token: String,
+    refresh_token: String,
+    token_type: String,
+    auto_login_id: String,
+    display_id: String,
+    main_device: String,
+    main_device_app_version: String,
 }
