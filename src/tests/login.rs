1b4ff564267670ba3f4ebd16cbd41d91f805d8b5
JellyBrick <shlee1503@naver.com>
2020-05-08 21:43:47 +0900

    Test: Change UUID to UWP version GUID

--- src/tests/login.rs
+++ src/tests/login.rs
@@ -1,13 +1,14 @@
 use crate::internal::{Client, LoginData};
+use data_encoding::BASE64;
 
 #[tokio::test]
 async fn login() {
     let client = Client::new();
     let login_data = LoginData::new(
-        "djdisodo@gmail.com".to_string(),
+        "test@gmail.com".to_string(),
         "password".to_string(),
-        "YWFhc2FmaGthc2hmamtoZGtmamFz".to_string(),
-        "djdisodo".to_string(),
+        BASE64.encode("02B9D784-75FE-47A7-AD47-C18B0AF11A1C".as_ref()),
+        "loco.rs".to_string(),
         false,
         "10.0".to_string()
     );
