c8e9fa54f418ed5730a553c0410b9ace41d7acfc
JellyBrick <shlee1503@naver.com>
2020-05-08 22:03:32 +0900

    Fix: Syntax error

--- src/internal/login_data.rs
+++ src/internal/login_data.rs
@@ -8,8 +8,8 @@ pub struct LoginData {
     device_uuid: String,
     device_name: String,
     os_version: String,
+    permanent: bool,
     forced: bool,
-    permanent: bool
 }
 
 impl LoginData {
@@ -19,9 +19,9 @@ impl LoginData {
             password,
             device_uuid,
             device_name,
-            os_version
-            forced,
+            os_version,
             permanent,
+            forced,
         };
     }
 
