a420d09d8e86a8802d82152a94ae12372a8c5c31
왕고슴도치 <djdisodo@gmail.com>
2020-05-11 21:44:40 +0900

    TokenClient: use blocking instead of async (#24)

--- src/internal/token_client.rs
+++ src/internal/token_client.rs
@@ -1,18 +1,19 @@
 use std::ops::Deref;
 
-use reqwest::{Error, Response};
+use reqwest::Error;
+use reqwest::blocking::Response;
 use serde_json;
 
 use crate::internal::{account, agent::Os, AUTH_USER_AGENT, DeviceRegisterData, LoginData};
 use crate::types::structs::login::LoginAccessData;
 
 pub struct TokenClient {
-    client: reqwest::Client,
+    client: reqwest::blocking::Client,
     agent: Os,
 }
 
 impl Deref for TokenClient {
-    type Target = reqwest::Client;
+    type Target = reqwest::blocking::Client;
 
     fn deref(&self) -> &Self::Target {
         return &self.client;
@@ -27,25 +28,24 @@ impl TokenClient {
         };
     }
 
-    pub async fn request_login(&self, login_data: &LoginData) -> Result<LoginAccessData, Error> {
+    pub fn request_login(&self, login_data: &LoginData) -> Result<LoginAccessData, Error> {
         let result: Result<Response, Error> = self.post(account::get_login_url(&self.agent))
             .headers(account::get_auth_header(
                 &self.agent,
                 &login_data.to_xvc_key(AUTH_USER_AGENT),
             ))
             .form(login_data)
-            .send()
-            .await;
+            .send();
 
         return match result {
             Ok(response) => {
-                Ok(serde_json::from_str(&response.text().await.unwrap()).unwrap())
+                Ok(serde_json::from_str(&response.text().unwrap()).unwrap())
             },
             Err(error) => Err(error),
         }
     }
 
-    pub async fn request_passcode(&self, login_data: &LoginData) -> Result<Response, Error> {
+    pub fn request_passcode(&self, login_data: &LoginData) -> Result<Response, Error> {
         self.post(account::get_request_passcode_url(&self.agent))
             .headers(account::get_auth_header(
                 &self.agent,
@@ -53,14 +53,12 @@ impl TokenClient {
             ))
             .form(login_data)
             .send()
-            .await
     }
 
-    pub async fn register_device(&self, device_register_data: &DeviceRegisterData) -> Result<Response, Error> {
+    pub fn register_device(&self, device_register_data: &DeviceRegisterData) -> Result<Response, Error> {
         self.post(account::get_register_device_url(&self.agent))
             .headers(account::get_auth_header(&self.agent, &device_register_data.to_xvc_key(AUTH_USER_AGENT)))
             .form(device_register_data)
             .send()
-            .await
     }
 }
