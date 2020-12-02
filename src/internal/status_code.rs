965b85c54cd6ce5f2fdd73e735ba8f6bf1a6e6e2
djdisodo <djdisodo@gmail.com>
2020-12-02 22:57:32 +0900

    add derive macro Clone, Copy to StatusCode

--- src/internal/status_code.rs
+++ src/internal/status_code.rs
@@ -1,5 +1,5 @@
 use serde_repr::*;
-#[derive(Serialize_repr, Deserialize_repr, PartialEq, Debug)]
+#[derive(Serialize_repr, Deserialize_repr, PartialEq, Debug, Clone, Copy)]
 #[repr(i32)]
 pub enum StatusCode {
 	Success = 0,
